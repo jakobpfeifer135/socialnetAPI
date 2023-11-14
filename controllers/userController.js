f90552ea8c24ccc041ed7d6b5edd5ba4abe0af67
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-11-14 17:29:41 -0600

    added controller code

---
+++ controllers/userController.js
@@ -0,0 +1,56 @@
+const User = require('../models/user');
+
+module.exports = {
+  async getUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select('-__v');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // create a new user
+  async createUser(req, res) {
+    try {
+      const dbUserData = await User.create(req.body);
+      res.json(dbUserData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async updateUser(req,res) {
+    try {
+        const user = await User.findOneAndUpdate(
+        {_id:req.params.userId},
+        {$set:req.body},
+        {runValidators:true, new:true}
+        
+        )
+        .then((user) => {
+            if (!user) {
+                return res.status(404).json({ message: 'No user with that ID' });
+              } else {
+                res.json(user)
+              } 
+              
+        })
+    }
+    catch (err) {
+        res.status(500).json(err);
+      }
+  },
+};
